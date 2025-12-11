f68c4e0b15a50d8e1f2b44841dffdc529b72841a
authenticcheckers <authenticcheckers@gmail.com>
2025-12-11 16:47:36 +0000

    Update route.js

--- app/api/products/route.js
+++ app/api/products/route.js
@@ -1,5 +1,5 @@
-import dbConnect from '../../lib/db';
-import Product from '../../models/Product';
+import dbConnect from '../../../lib/db';
+import Product from '../../../models/Product';
 import { NextResponse } from 'next/server';
 
 export async function GET() {
