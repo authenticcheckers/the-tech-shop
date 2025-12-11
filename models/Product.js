126322e92caa91880fbf92be67a09463295584a2
authenticcheckers <authenticcheckers@gmail.com>
2025-12-11 10:43:14 +0000

    Create Product.js

---
+++ models/Product.js
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose';
+
+const ProductSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  price: { type: Number, required: true },
+  image: { type: String, required: true },
+  category: { type: String, default: 'Tech' },
+  description: String,
+});
+
+export default mongoose.models.Product || mongoose.model('Product', ProductSchema);
