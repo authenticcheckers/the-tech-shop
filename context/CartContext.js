41cc95e030c8de44dd1b3a6424be192636c3447f
authenticcheckers <authenticcheckers@gmail.com>
2025-12-11 10:45:06 +0000

    Create CartContext.js

---
+++ context/CartContext.js
@@ -0,0 +1,26 @@
+"use client";
+import { createContext, useContext, useState } from 'react';
+
+const CartContext = createContext();
+
+export function CartProvider({ children }) {
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (product) => {
+    setCart((prev) => [...prev, product]);
+  };
+
+  const removeFromCart = (productId) => {
+    setCart((prev) => prev.filter((item) => item._id !== productId));
+  };
+
+  const total = cart.reduce((acc, item) => acc + item.price, 0);
+
+  return (
+    <CartContext.Provider value={{ cart, addToCart, removeFromCart, total }}>
+      {children}
+    </CartContext.Provider>
+  );
+}
+
+export const useCart = () => useContext(CartContext);
