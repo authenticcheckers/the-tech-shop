419017b2875c503cf9206426d48ca8599a5f4a4f
authenticcheckers <authenticcheckers@gmail.com>
2025-12-11 18:25:52 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,7 +6,12 @@ const withTM = require('next-transpile-modules')([
   '@clerk/nextjs'
 ]);
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+const withCSS = require('@zeit/next-css') // Import the CSS helper
 
-module.exports = withTM(nextConfig);
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Add any specific next.js configuration here
+};
+
+// Chain the configurations: first the transpiler, then the CSS handler
+module.exports = withTM(withCSS(nextConfig));
