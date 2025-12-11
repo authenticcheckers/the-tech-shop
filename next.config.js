ffae674cb9f32b8cc313eb7509ebec88cd952f17
authenticcheckers <authenticcheckers@gmail.com>
2025-12-11 18:00:26 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,16 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Transpile the specific Clerk package causing the Edge Function crash
-transpilePackages: ['@clerk/shared', '@clerk/clerk-react', '@clerk/nextjs'],
-
-  // This is the core fix for the Edge compatibility using the proxy
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      // We instruct Next.js to use the proxy file only when compiling for the client/edge environment
-      config.resolve.alias['@clerk/shared'] = require.resolve('./proxy.ts');
-    }
-    return config;
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
